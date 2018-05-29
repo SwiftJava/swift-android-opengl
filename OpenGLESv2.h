d4534ce59024a05012aa583a0a0ba31f7c951232
John Holdsworth <github@johnholdsworth.com>
2018-05-29 18:34:55 +0100

    OpenGLESv2 SPM module

---
+++ OpenGLESv2.h
@@ -0,0 +1,9 @@
+//
+//  OpenGLESv2.h
+//  OpenGLESv2
+//
+//  Created by John Holdsworth on 20/07/2016.
+//  Copyright (c) 2016 John Holdsworth. All rights reserved.
+//
+
+#import <GLES2/gl2.h>
---
+++ README.md
@@ -0,0 +1,3 @@
+# OpenGLESv2 Swift package
+
+Allows using `OpenGLESv2` in other Swift packages.
